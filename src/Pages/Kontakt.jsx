acfeb1d52f7b270c65c626c24096fbb861c24be8
fabiobrauer <brauerfabiio@gmail.com>
2023-11-21 11:10:54 +0100

    test 4

--- src/Pages/Kontakt.jsx
+++ src/Pages/Kontakt.jsx
@@ -3,25 +3,21 @@ import './Kontakt.css'
 
 function Kontakt() {
   return (
-    <>
-      <h1>Kontaktieren Sie mich</h1>       
-      <div className="contact-form">
-          <form name="contact" data-netlify="true">
-            <p className="form-group">
-              <label>Name <input type="text" name="name" /></label>
-            </p>
-            <p className="form-group">
-              <label>Email <input type="email" name="email" /></label>
-            </p>
-            <p className="form-group">
-              <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-      </div>
-    </>
+    <form name="contact" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+          <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+          <button type="submit">Send</button>
+      </p>
+    </form>
   )
 }
 
